be30aff569ad3a9cc4c6b6ed2033b71cbd62e838
Marcelo Jacubowski <marcelosj99@gmail.com>
2020-04-28 16:32:19 -0300

    Piece move count and fixed the double printing of the current player

--- src/application/UI.java
+++ src/application/UI.java
@@ -70,7 +70,7 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        //System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
         if (!chessMatch.getCheckMate()){
             System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
             if (chessMatch.getCheck()){
