d7aed632c12e8f071bf8ccb41ad72e9ba7ae12cc
Marcelo Jacubowski <marcelosj99@gmail.com>
2020-04-28 11:45:03 -0300

    Fixed the stucked piece and implementing possible moves of rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,6 +31,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -44,18 +45,24 @@ public class ChessMatch {
 
     private void validateSourcePosition(Position position){
         if (!board.thereIsAPiece(position)){
-            throw new ChessException("Theres is no piece on source position");
+            throw new ChessException("There is no piece on source position");
         }
 
-        if (board.piece(position).isThereAnyPossibleMoves()){
+        if (!board.piece(position).isThereAnyPossibleMoves()){
             throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+    }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
+
 
     private void initialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
